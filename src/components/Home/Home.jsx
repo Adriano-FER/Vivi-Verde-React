82e9aebf5b94b3bf740d04e549c0547b4eb93183
Adriano-FER <adrianofp-94@hotmail.es>
2023-09-17 18:53:27 -0300

    modificado register

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="home">
-      <h1>Welcome to my website!</h1>
+      <h1>Welcome to JOHANNA CODEA</h1>
       <p>This is a simple but minimalistic website.</p>
     </div>
   );
